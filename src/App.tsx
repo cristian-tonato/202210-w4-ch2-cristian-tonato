0a6b14c1e4e3af450e01344387c637424d08b59b
Cristian <cristianotona@gmail.com>
2022-10-25 19:16:44 +0200

    firstpage

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,8 @@ function App() {
     return (
         <>
         <header></header>
-        <body>
+        
             <PersonalData></PersonalData>
-        </body>
         
         </>
     );
