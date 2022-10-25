1e3d59c711d37266133894ff890e5ba07365f0b3
Cristian <cristianotona@gmail.com>
2022-10-26 00:31:36 +0200

    components/form

--- src/components/formulario/formulario.tsx
+++ src/components/formulario/formulario.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
 
-type Personal= {
+type Personal = {
     name: string;
     lastName: string;
     birthday: number;
     gender: string;
     email: string;
     info: boolean;
-}
+    username: string;
+    password: string;
+    repeatPassword:string;
+    account: string;
+};
 
 export function PersonalData() {
     const personal: Personal ={
@@ -17,16 +21,23 @@ export function PersonalData() {
     birthday: 0,
     gender: "",
     email: "",
-    info: false
+    info: false,
+    username: "",
+    password:"",
+    repeatPassword:"",
+    account: ""
     }
 
+    const [counter,setCounter]= useState(0);
+    const handlerCounter= (value:number)=>{
+        setCounter(counter+value)
+    }
     const[form,setForm]= useState(personal);
     
     const handlerSubmit = (ev: any) => {
         ev.preventDefault();
         console.log("Enviando");
     }
-
         const handleForm = (ev: any) => {
             const element = ev.target as HTMLFormElement;
             setForm({
@@ -36,96 +47,191 @@ export function PersonalData() {
                         ? element.checked
                         : element.value,
             });
-        };
-    
-    return (
-        <>
-            <form onSubmit={handlerSubmit}>
-                <legend>FORM</legend>
-                <div>
-                    <label>
-                        NAME:{" "}
+        }
+        const persData=()=>{
+            const template = (
+                <>
+                    <form onSubmit={handlerSubmit}>
+                        <legend>FORM</legend>
+                        <div>
+                            <label>
+                                NAME:{" "}
+                                <input
+                                    type="text"
+                                    name="name"
+                                    placeholder="Name"
+                                    value={form.name}
+                                    onInput={handleForm}
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                LASTNAME:
+                                <input
+                                    type="text"
+                                    name="lastName"
+                                    placeholder="Last Name"
+                                    value={form.lastName}
+                                    onInput={handleForm}
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                BD
+                                <input
+                                    type="date"
+                                    name="birthday"
+                                    placeholder="Birthday"
+                                    value={form.birthday}
+                                    onInput={handleForm}
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <legend>GENDER</legend>
+                            <fieldset>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="gender"
+                                        value="male"
+                                        onChange={handleForm}
+                                    />
+                                    MALE
+                                    <input
+                                        type="radio"
+                                        name="gender"
+                                        value="female"
+                                        onChange={handleForm}
+                                    />
+                                    FEMALE
+                                </label>
+                            </fieldset>
+                        </div>
+                        <div>
+                            <label>
+                                EMAIL{" "}
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="email"
+                                    value={form.email}
+                                    onInput={handleForm}
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    name="info"
+                                    checked={form.info}
+                                    onChange={handleForm}
+                                />
+                                NEWSLETTER
+                            </label>
+                        </div>
+                        <div>
+                            <button type="submit">ENVIAR</button>
+                        </div>
+                        <div>
+                            <button
+                                type="submit"
+                                onClick={() => {
+                                    handlerCounter(+1);
+                                }}
+                            >
+                                Next
+                            </button>
+                        </div>
+                    </form>
+                </>
+            );
+        
+    return template
+       }
+       const accessData=()=>{
+        const template=( 
+            <>
+                <form onSubmit={handlerSubmit}>
+                    <legend>Formulario</legend>
+                    <div>
                         <input
                             type="text"
-                            name="name"
-                            placeholder="Name"
-                            value={form.name}
+                            name="username"
                             onInput={handleForm}
+                            placeholder="Username"
                         />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        LASTNAME:
+                    </div>
+                    <div>
                         <input
-                            type="text"
-                            name="lastName"
-                            placeholder="Last Name"
-                            value={form.lastName}
-                            onInput={handleForm}
+                            type="password"
+                            name="password"
+                            onChange={handleForm}
+                            placeholder="password"
+                            value={form.password}
                         />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        BD
-                        <input
-                            type="text"
-                            name="birthday"
-                            placeholder="Birthday"
-                            value={form.birthday}
+                    </div>
+                    <div>
+                        <legend>Acount type</legend>
+                        <select name="acount" required>
+                            <option value="PER">PERSONAL</option>
+                            <option value="PRO">PRO</option>
+                            <option value="BUS">BUSSINES</option>
+                        </select>
+                    </div>
+                    <div>
+                        <button
                             onInput={handleForm}
-                        />
-                    </label>
-                </div>
-                <div>
-                    <legend>GENDER</legend>
-                    <fieldset>
-                        <label>
-                            <input
-                                type="radio"
-                                name="gender"
-                                value={form.gender}
-                                onInput={handleForm}
-                            />
-                            MALE
-                            <input
-                                type="radio"
-                                name="gender"
-                                value={form.gender}
-                                onInput={handleForm}
-                            />
-                            FEMALE
-                        </label>
-                    </fieldset>
-                </div>
-                <div>
-                    <label>
-                        EMAIL{" "}
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="email"
-                            value={form.email}
+                            onClick={() => {
+                                handlerCounter(-1);
+                            }}
+                        >
+                            Previous
+                        </button>
+                        <button
                             onInput={handleForm}
-                        />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        <input
-                            type="checkbox"
-                            name="info"
-                            checked={form.info}
-                            onInput={handleForm}
-                        />
-                        NEWSLETTER
-                    </label>
-                </div>
-                <div>
-                    <button type="submit">ENVIAR</button>
-                </div>
-            </form>
-        </>
-    );
-}
+                            onClick={() => {
+                                handlerCounter(1);
+                            }}
+                        >
+                            Continue
+                        </button>
+                    </div>
+                </form>
+            </>
+        );
+        return template
+    }
+    const confirmation=()=>{
+        const template = (
+            <>
+            <div>
+                <h2>CONFIRMATION</h2>
+                <p>Name:{form.name} </p>
+                <p>Last Name:{form.lastName}</p>
+                <p>Birthday:{form.birthday}</p>
+                <p>Gender:{form.gender}</p>
+                <p>Email:{form.email}</p>
+                <p>Username:{form.username}</p>
+                <p>Account type:{form.account} </p>
+                <p>Password:{form.password}</p>
+            </div>
+            <div>
+                <button type="submit" onClick={()=>{
+                    handlerCounter(-1)
+                }}
+                
+                >PREVious</button>
+            </div>
+            </>
+        );
+        return template
+    };
+     if (counter === 0) return persData();
+     if (counter === 1) return accessData();
+     if (counter === 2) return confirmation();
+     return <></>;
+};
